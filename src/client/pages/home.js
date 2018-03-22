ccf39392099049d9895c2eb7713a290c31d0e0a9
Nic Bonetto <nick.bonetto@gmail.com>
2018-03-22 15:58:16 -0700

    Created page layout and home page. #1

---
+++ src/client/pages/home.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+import Page from '../layouts/page';
+
+const Home = props => {
+  return (
+    <Fragment>
+      <Page/>
+    </Fragment>
+  )
+}
+
+export default Home;
