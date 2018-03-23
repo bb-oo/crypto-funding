e921b80776af2bb85d0b3a1a7eddc6a9eab56811
Nic Bonetto <nick.bonetto@gmail.com>
2018-03-22 21:44:41 -0700

    Added components to Home component. #2

--- src/client/components/index.js
+++ src/client/components/index.js
@@ -1,6 +1,7 @@
+import Header from './header';
 import Jumbo from './jumbo';
 import NavBar from './nav-bar';
 import NavLink from './nav-link';
 import Routers from './router';
 
-export { Jumbo, NavBar, NavLink, Routers };
+export { Jumbo, NavBar, NavLink, Routers, Header };
--- src/client/pages/home.js
+++ src/client/pages/home.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 
-import { NavBar, Jumbo } from '../components/index';
+import { NavBar, Jumbo, Header } from '../components/index';
 
 const Home = props => {
   return (
     <Fragment>
       <NavBar/>
       <Jumbo bg="linear-gradient(to right, #4b74b3, #4f4f8d)">
+        <Header text="Invest for your future." font="Open Sans, sans-serif" weight="400" size={ 1 } mt="185px"/>
+        <Header text="Backers FundMe is a revolutionary platform that enables you to campaign with cryptocurrencies." font="Open Sans, sans-serif" weight="400" size={ 4 }/>
       </Jumbo>
     </Fragment>
   )
