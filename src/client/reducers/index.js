afb5b66243030ffa10185b4d3f4d6541c92dc9db
Nic Bonetto <nick.bonetto@gmail.com>
2018-03-26 10:50:57 -0700

    Added redux form reducer. #3

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,5 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import session from './session';
 
-export default combineReducers({ session })
+export default combineReducers({ 
+  session: session,
+  form: formReducer
+});
