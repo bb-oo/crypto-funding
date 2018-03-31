32c6adfeb39c1496daa58a15c28a44f3128881b5
Nic Bonetto <nick.bonetto@gmail.com>
2018-03-31 15:14:27 -0700

    Added new postgres campaign table. #9

---
+++ src/database/campaigns.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS campaigns;
+
+CREATE TABLE campaigns (
+  id           serial,
+  title        text,
+  organizer    text,
+  organizer_id int,
+  description  text,
+  target       text,
+  zip          int,
+  category     text,
+  donations    int,
+  goal         decimal
+);
