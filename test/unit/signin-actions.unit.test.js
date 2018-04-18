a8d0906a7075b31a37445e31384fc5391e717ff4
Nic Bonetto <nick.bonetto@gmail.com>
2018-04-17 19:30:52 -0700

    Only adding onSubmit test for now. #24

--- test/unit/signin-actions.unit.test.js
+++ test/unit/signin-actions.unit.test.js
@@ -2,28 +2,28 @@ import { describe, it, beforeEach, afterEach } from 'mocha';
 import { expect, stub, injectStore } from '../utils/unit';
 import axios from 'axios';
 
-import { onSubmit, onSubmitSuccess } from '../../src/client/authentication/actions';
+import { onSubmit } from '../../src/client/authentication/actions';
 
 describe('authentication/actions', () => {
   
-    let store;
+  let store;
 
-    beforeEach(() => {
-      store = injectStore()();
+  beforeEach(() => {
+    store = injectStore()();
+  })
+
+  describe('onSubmit', () => {
+
+    beforeEach(() => stub(axios, 'post'))
+
+    afterEach(() => axios.post.restore())
+
+    it('posts a set of user credentials', async () => {
+      const creds = { username: 'testUser', email: 'test@testing.com', password: 'testing123' };
+      axios.post.resolves({ data: '' });
+      const res = await store.dispatch(onSubmit(creds));
+
+      expect(res).to.deep.equal(creds);
     })
-
-    describe('onSubmit', () => {
-
-      beforeEach(() => stub(axios, 'post'))
-
-      afterEach(() => axios.post.restore())
-
-      it('posts a set of user credentials', async () => {
-        const creds = { username: 'testUser', email: 'test@testing.com', password: 'testing123' };
-        axios.post.resolves({ data: '' });
-        const res = await store.dispatch(onSubmit(creds));
-
-        expect(res).to.deep.equal(creds);
-      })
-    })
+  })
 })
